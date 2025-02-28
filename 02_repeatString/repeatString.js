7dfc1d78cfd8b05c38fec6c3bd5103f791fdafce
Kam Kennedy <kameronk013@gmail.com>
2025-02-28 10:34:55 -0700

    first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const { TiMessage } = require("react-icons/ti");
 
+const repeatString = function(word, n) {
+  if (n < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < n; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
